5be58d2e08f40f38abf00e1fa7c559d6d2aec3b7
Hidden Refuge <hidden-refuge@users.noreply.github.com>
2015-08-30 23:41:45 +0200

    Update bench.sh

--- bench.sh
+++ bench.sh
@@ -120,7 +120,7 @@ iotest () {
 	ioraw=$( echo $io | awk 'NR==1 {print $1}' )
 	ioraw2=$( echo $io2 | awk 'NR==1 {print $1}' )
 	ioraw3=$( echo $io3 | awk 'NR==1 {print $1}' )
-	ioall=$( expr $io + $io2 + $io3)
+	ioall=$( expr $ioraw + $ioraw2 + $ioraw3 )
 	ioavg=$( expr $ioall / 3 )
 	echo "Average I/O: $ioavg MB/s"
 	echo ""
